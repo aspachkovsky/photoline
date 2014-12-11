5964bab821c39e46865c4c6e794cb34f9c245a70
aspachkovsky <pbl.symbolist@gmail.com>
2014-12-11 18:58:51 +0300

    codelite installation script added

---
+++ scripts/codelite-install.sh
@@ -0,0 +1,4 @@
+apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
+apt-add-repository 'deb http://repos.codelite.org/ubuntu/ trusty universe'
+apt-get update
+apt-get install codelite wxcrafter
